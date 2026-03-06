cc741e6829d3d446ace000440352bc513f2779df
Oscar GP <90568125+OscarJGP@users.noreply.github.com>
2026-03-06 08:03:29 -0600

    Fix array size and memory address print statements

--- Arreglos/Arreglo01.c
+++ Arreglos/Arreglo01.c
@@ -3,10 +3,10 @@
 
 int main (){
     float float1, float2;
-    float temp[24];
+    float temp[23];
     int dummy;
     printf ("Direccion de memoria de dos flotantes \n &temp1 %i \n &temp2 %i \n", &float1, &float2);
-    printf ("Direccion de memoria de &float [24] %d\n", &temp[24]);
+    printf ("Direccion de memoria de &float [23] %d\n", &temp[23]);
     printf ("Direccion de memoria de float  %d\n", temp);
     printf ("Direccion de memoria de dummy %d\n", &dummy);
 
